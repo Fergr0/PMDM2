017d544f5f5330190835185ced3022ffac64e7c7
fergr01 <fergr01bbx@gmail.com>
2025-01-09 20:02:13 +0100

    9_1lista

---
+++ propuesta9_1/app/src/main/java/com/example/propuesta9_1/Fragmento1.java
@@ -0,0 +1,49 @@
+package com.example.propuesta9_1;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Fragmento1 extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public Fragmento1() {
+        // Required empty public constructor
+    }
+
+    public static Fragmento1 newInstance(String param1, String param2) {
+        Fragmento1 fragment = new Fragmento1();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragmento1, container, false);
+    }
+}
