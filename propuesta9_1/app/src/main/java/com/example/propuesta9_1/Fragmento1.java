b9c141ce65f8e06433da14d8ad0b906fe20b363c
Fergr0 <fergr01bbx@gmail.com>
2025-01-10 11:07:39 +0100

    clase

--- propuesta9_1/app/src/main/java/com/example/propuesta9_1/Fragmento1.java
+++ propuesta9_1/app/src/main/java/com/example/propuesta9_1/Fragmento1.java
@@ -14,7 +14,7 @@ public class Fragmento1 extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
+
     private String mParam1;
     private String mParam2;
 
