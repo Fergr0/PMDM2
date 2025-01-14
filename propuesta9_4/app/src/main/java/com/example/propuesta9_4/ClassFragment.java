0d9fe2d1aeecd251c4fb8002b5c83764446b24ec
Fergr0 <fergr01bbx@gmail.com>
2025-01-14 09:31:00 +0100

    poner 9_4

---
+++ propuesta9_4/app/src/main/java/com/example/propuesta9_4/ClassFragment.java
@@ -0,0 +1,64 @@
+package com.example.propuesta9_4;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.propuesta9_4.placeholder.PlaceholderContent;
+
+/**
+ * A fragment representing a list of Items.
+ */
+public class ClassFragment extends Fragment {
+
+
+    private static final String ARG_COLUMN_COUNT = "column-count";
+
+    private int mColumnCount = 1;
+
+
+    public ClassFragment() {
+    }
+
+    // TODO: Customize parameter initialization
+    @SuppressWarnings("unused")
+    public static ClassFragment newInstance(int columnCount) {
+        ClassFragment fragment = new ClassFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_COLUMN_COUNT, columnCount);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_item_list, container, false);
+
+        // Set the adapter
+        if (view instanceof RecyclerView) {
+            Context context = view.getContext();
+            RecyclerView recyclerView = (RecyclerView) view;
+            if (mColumnCount <= 1) {
+                recyclerView.setLayoutManager(new LinearLayoutManager(context));
+            } else {
+                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
+            }
+            recyclerView.setAdapter(new MyClassRecyclerViewAdapter(PlaceholderContent.ITEMS));
+        }
+        return view;
+    }
+}
